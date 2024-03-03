318082d4ccd06de11b5bd935ee69e73643383e6a
Mohit Chauhan <mohitchauhan0226@gmail.com>
2024-03-03 18:16:59 +0530

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
-import {preprocess} from "svelte-preprocess";
+import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 
 
 
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		preprocess: preprocess(),
-		adapter: adapter(),
+		adapter: adapter(
+			{
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: false
+			}
+		),
 	},
 };
 export default config;
